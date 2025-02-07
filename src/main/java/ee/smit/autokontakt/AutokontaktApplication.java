14cea91c732e64f9babace0be0b0033da5761c11
Marko Niitsoo <marko.niitsoo@gmail.com>
2025-02-07 14:34:13 +0200

    initialize Spring Boot project with basic structure and configuration

---
+++ src/main/java/ee/smit/autokontakt/AutokontaktApplication.java
@@ -0,0 +1,13 @@
+package ee.smit.autokontakt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutokontaktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutokontaktApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/smit/autokontakt/AutokontaktApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.smit.autokontakt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutokontaktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
