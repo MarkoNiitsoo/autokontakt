8b4cc48a83cfdb105cc21f531a4d47a75bb42797
Marko Niitsoo <marko.niitsoo@gmail.com>
2025-02-11 19:56:10 +0200

    refactor data.sql to insert car brands and their hierarchical relationships more clearly

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,37 +1,39 @@
+-- Insert top-level brands first
 INSERT INTO car_brand (name, parent_id) VALUES ('Mercedes-Benz', NULL);
-INSERT INTO car_brand (name, parent_id) VALUES ('C klass', (SELECT id FROM car_brand WHERE name='Mercedes-Benz'));
-INSERT INTO car_brand (name, parent_id) VALUES ('C 160', (SELECT id FROM car_brand WHERE name='C klass'));
-INSERT INTO car_brand (name, parent_id) VALUES ('C 180', (SELECT id FROM car_brand WHERE name='C klass'));
-INSERT INTO car_brand (name, parent_id) VALUES ('C 200', (SELECT id FROM car_brand WHERE name='C klass'));
-INSERT INTO car_brand (name, parent_id) VALUES ('C 220', (SELECT id FROM car_brand WHERE name='C klass'));
-
 INSERT INTO car_brand (name, parent_id) VALUES ('BMW', NULL);
-INSERT INTO car_brand (name, parent_id) VALUES ('3 seeria', (SELECT id FROM car_brand WHERE name='BMW'));
-INSERT INTO car_brand (name, parent_id) VALUES ('315', (SELECT id FROM car_brand WHERE name='3 seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('317', (SELECT id FROM car_brand WHERE name='3 seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('319', (SELECT id FROM car_brand WHERE name='3 seeria'));
-
-INSERT INTO car_brand (name, parent_id) VALUES ('4 seeria', (SELECT id FROM car_brand WHERE name='BMW'));
-
-INSERT INTO car_brand (name, parent_id) VALUES ('5 seeria', (SELECT id FROM car_brand WHERE name='BMW'));
-INSERT INTO car_brand (name, parent_id) VALUES ('518', (SELECT id FROM car_brand WHERE name='5 seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('523', (SELECT id FROM car_brand WHERE name='5 seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('524', (SELECT id FROM car_brand WHERE name='5 seeria'));
-
 INSERT INTO car_brand (name, parent_id) VALUES ('Audi', NULL);
-INSERT INTO car_brand (name, parent_id) VALUES ('e-tron', (SELECT id FROM car_brand WHERE name='Audi'));
-
-INSERT INTO car_brand (name, parent_id) VALUES ('Q seeria', (SELECT id FROM car_brand WHERE name='Audi'));
-INSERT INTO car_brand (name, parent_id) VALUES ('Q2', (SELECT id FROM car_brand WHERE name='Q seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('Q4', (SELECT id FROM car_brand WHERE name='Q seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('Q5', (SELECT id FROM car_brand WHERE name='Q seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('Q7', (SELECT id FROM car_brand WHERE name='Q seeria'));
-
-INSERT INTO car_brand (name, parent_id) VALUES ('RS seeria', (SELECT id FROM car_brand WHERE name='Audi'));
-INSERT INTO car_brand (name, parent_id) VALUES ('RS4', (SELECT id FROM car_brand WHERE name='RS seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('RS5', (SELECT id FROM car_brand WHERE name='RS seeria'));
-INSERT INTO car_brand (name, parent_id) VALUES ('RS6', (SELECT id FROM car_brand WHERE name='RS seeria'));
-
-INSERT INTO car_brand (name, parent_id) VALUES ('TT', (SELECT id FROM car_brand WHERE name='Audi'));
-
 INSERT INTO car_brand (name, parent_id) VALUES ('Citroën', NULL);
+
+-- Then, insert children, referencing the parent IDs
+INSERT INTO car_brand (name, parent_id) VALUES ('C klass', 1);
+INSERT INTO car_brand (name, parent_id) VALUES ('C 160', 5);
+INSERT INTO car_brand (name, parent_id) VALUES ('C 180', 5);
+INSERT INTO car_brand (name, parent_id) VALUES ('C 200', 5);
+INSERT INTO car_brand (name, parent_id) VALUES ('C 220', 5);
+
+INSERT INTO car_brand (name, parent_id) VALUES ('3 seeria', 2);
+INSERT INTO car_brand (name, parent_id) VALUES ('315', 10);
+INSERT INTO car_brand (name, parent_id) VALUES ('317', 10);
+INSERT INTO car_brand (name, parent_id) VALUES ('319', 10);
+
+INSERT INTO car_brand (name, parent_id) VALUES ('4 seeria', 2);
+
+INSERT INTO car_brand (name, parent_id) VALUES ('5 seeria', 2);
+INSERT INTO car_brand (name, parent_id) VALUES ('518', 15);
+INSERT INTO car_brand (name, parent_id) VALUES ('523', 15);
+INSERT INTO car_brand (name, parent_id) VALUES ('524', 15);
+
+INSERT INTO car_brand (name, parent_id) VALUES ('e-tron', 3);
+
+INSERT INTO car_brand (name, parent_id) VALUES ('Q seeria', 3);
+INSERT INTO car_brand (name, parent_id) VALUES ('Q2', 20);
+INSERT INTO car_brand (name, parent_id) VALUES ('Q4', 20);
+INSERT INTO car_brand (name, parent_id) VALUES ('Q5', 20);
+INSERT INTO car_brand (name, parent_id) VALUES ('Q7', 20);
+
+INSERT INTO car_brand (name, parent_id) VALUES ('RS seeria', 3);
+INSERT INTO car_brand (name, parent_id) VALUES ('RS4', 25);
+INSERT INTO car_brand (name, parent_id) VALUES ('RS5', 25);
+INSERT INTO car_brand (name, parent_id) VALUES ('RS6', 25);
+
+INSERT INTO car_brand (name, parent_id) VALUES ('TT', 3);
