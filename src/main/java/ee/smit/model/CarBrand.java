2b40ba3b4dc3ff3fdec5c6767ee2e5d992d27f96
Marko Niitsoo <marko.niitsoo@gmail.com>
2025-02-11 19:55:03 +0200

    add CarBrand entity with table mapping for JPA

--- src/main/java/ee/smit/model/CarBrand.java
+++ src/main/java/ee/smit/model/CarBrand.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "car_brand")
 public class CarBrand {
     
     @Id
