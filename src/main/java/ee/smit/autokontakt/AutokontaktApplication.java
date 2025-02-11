97724b0f0dc233f8d82621a4b1d5dbeca2b0928f
Marko Niitsoo <marko.niitsoo@gmail.com>
2025-02-11 19:44:53 +0200

    configure Spring Boot application for component scanning and JPA repository support

--- src/main/java/ee/smit/autokontakt/AutokontaktApplication.java
+++ src/main/java/ee/smit/autokontakt/AutokontaktApplication.java
@@ -2,8 +2,14 @@ package ee.smit.autokontakt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ee.smit")
+@EnableJpaRepositories(basePackages = "ee.smit.repository")
+@EntityScan(basePackages = "ee.smit.model")
 public class AutokontaktApplication {
 
 	public static void main(String[] args) {
