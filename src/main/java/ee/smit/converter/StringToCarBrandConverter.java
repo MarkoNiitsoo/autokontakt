0ec1bbb2759c28be76010156f84399711fbc10b4
Marko Niitsoo <marko.niitsoo@gmail.com>
2025-02-11 19:55:28 +0200

    add StringToCarBrandConverter to convert String IDs to CarBrand entities

---
+++ src/main/java/ee/smit/converter/StringToCarBrandConverter.java
@@ -0,0 +1,26 @@
+package ee.smit.converter;
+
+import ee.smit.model.CarBrand;
+import ee.smit.repository.CarBrandRepository;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToCarBrandConverter implements Converter<String, CarBrand> {
+
+    private final CarBrandRepository carBrandRepository;
+
+    public StringToCarBrandConverter(CarBrandRepository carBrandRepository) {
+        this.carBrandRepository = carBrandRepository;
+    }
+
+    @Override
+    public CarBrand convert(@NonNull String source) {
+        if (source == null || source.isEmpty()) {
+            return null;
+        }
+        return carBrandRepository.findById(Long.parseLong(source))
+                .orElseThrow(() -> new IllegalArgumentException("CarBrand not found with id: " + source));
+    }
+}
